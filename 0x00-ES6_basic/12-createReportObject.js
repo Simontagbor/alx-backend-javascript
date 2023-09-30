d0499d780de2990e1b2693cddd3cd94c444f8431
Simontagbor <simontagbor360@gmail.com>
2023-09-30 14:38:58 +0000

    fix tabspacing

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,14 +1,16 @@
 export default function createReportObject(employeesList) {
-  const report = {
-    "allEmployees": employeesList,
-    getNumberOfDepartments() {
-      const department = [];
-      for (const value of Object.keys(employeesList)) {
-        department.push(value);
-      }
-      const dept_count = new Set(department).size;
-      return dept_count;
-    }
-  };
-  return report;
+    const report = {
+        "allEmployees": employeesList,
+        getNumberOfDepartments() {
+            const department = [];
+	    
+            for (const value of Object.keys(employeesList)) {
+		 department.push(value);
+            }
+            const dept_count = new Set(department).size;
+            return dept_count; 
+	}
+    };
+    return report;
 }
+
