8ab86c4cfe71b8be0afaac14e31e2ece377dcde9
Simontagbor <simontagbor360@gmail.com>
2023-10-07 14:36:25 +0000

    create a new interface for Directors using an extension of the Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,14 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any; // Allow additional attributes with any name and type
+  }
+
+  interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+
+
