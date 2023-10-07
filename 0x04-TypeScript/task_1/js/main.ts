f9a2121fae0312f9493bccc588651c5e2fa3569d
Simontagbor <simontagbor360@gmail.com>
2023-10-07 14:50:17 +0000

    add function to print teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+
 interface Teacher {
   firstName: string;
   lastName: string;
@@ -11,4 +12,42 @@ interface Teacher {
     numberOfReports: number;
   }
 
+  interface printTeacherFunction {
+    (firstName: string, lastName: string): string;  
+  }
+
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+  const firstNameInitial = firstName.charAt(0).toUpperCase();
+  return `${firstNameInitial}. ${lastName}`;
+  };
+
+  interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+interface Student {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements Student {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Export the class
+export { StudentClass, StudentConstructor };
 
--- 0x04-TypeScript/task_1/main.ts
+++
@@ -1,8 +0,0 @@
-interface Teacher {
-  firstName: string;
-  lastName: string;
-  readonly fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  readonly location: string;
-  [key: string]: any; // Allow additional attributes with any name and type
-}
