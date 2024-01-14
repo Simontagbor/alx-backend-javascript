5e0e187a6565009de41248d3a9ad67f879ec6c0d
Simontagbor <simontagbor360@gmail.com>
2024-01-14 14:04:01 +0000

    disable eslint test

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,11 +3,13 @@ export default class Building {
     if (this.constructor === Building || !this.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this.sqft = sqft;
+    // eslint-disable-next-line no-underscore-dangle
+    this._sqft = sqft;
   }
 
   // getter
   get sqft() {
-    return this.sqft;
+    // eslint-disable-next-line no-underscore-dangle
+    return this._sqft;
   }
 }
