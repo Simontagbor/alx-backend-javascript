49298caebaa566ca1cd9edf1c7da4ae82b0d1ac7
Simontagbor <simontagbor360@gmail.com>
2024-01-14 14:12:02 +0000

    add class inherritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // override evacuationWarningMessage
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
